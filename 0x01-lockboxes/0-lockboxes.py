a7afdf9ff696b98937d43cd2f7ee843a71cdb9a7
MichaelKazembe <michaelkazembe@gmail.com>
2023-12-07 15:22:10 +0200

    refactored code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
 """
-0. Lockboxes
-mandatory
-You have n number of locked boxes in front of you. Each box is numbered
-sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
-
-Write a method that determines if all the boxes can be opened.
-
-Prototype: def canUnlockAll(boxes)
-boxes is a list of lists
-A key with the same number as a box opens that box
-You can assume all keys will be positive integers
-There can be keys that do not have boxes
-The first box boxes[0] is unlocked
-Return True if all boxes can be opened, else return False
+lockboxes in python3
 """
 
 
 def canUnlockAll(boxes):
-    # Create a set to keep track of the keys we have
-    keys = {0}
-    # Iterate through the lockboxes
-    for i, box in enumerate(boxes):
-        # Check if we have the key to open the current lockbox
-        if i in keys or any(key in keys for key in box):
-            # Add the keys from the current lockbox to our set
-            keys.update(box)
-        else:
-            # If we don't have the key to open the current lockbox
+    """
+    Function that checks with a boolean value if the list type and
+    length to invoke two for iterations: one to traverse the list
+    and the other to compare if the key is idx or not in order to open
+    """
+    if not isinstance(boxes, list):
+        return False
+
+    for key in range(1, len(boxes) - 1):
+        boxes_checked = any(key in box and key != idx for idx,
+                            box in enumerate(boxes))
+        if not boxes_checked:
             return False
 
-    # If we successfully opened all lockboxes, return True
     return True
