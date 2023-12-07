0e589553c500ae9eae357a3e6847da79dcc5dce9
MichaelKazembe <michaelkazembe@gmail.com>
2023-12-07 15:08:29 +0200

    pep8 applied to code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -20,14 +20,14 @@ Return True if all boxes can be opened, else return False
 def canUnlockAll(boxes):
     # Create a set to keep track of the keys we have
     keys = {0}
-        # Iterate through the lockboxes
+    # Iterate through the lockboxes
     for i, box in enumerate(boxes):
         # Check if we have the key to open the current lockbox
         if i in keys or any(key in keys for key in box):
             # Add the keys from the current lockbox to our set
             keys.update(box)
         else:
-            # If we don't have the key to open the current lockbox, return False
+            # If we don't have the key to open the current lockbox
             return False
 
     # If we successfully opened all lockboxes, return True
