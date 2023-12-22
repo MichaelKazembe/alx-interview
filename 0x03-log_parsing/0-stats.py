6ff2acc45bc7be8545f6d031a11ee367038e1dde
Michaelkazembe <Michaelkazembe@gmail.com>
2023-12-21 16:12:39 -0800

    Bug fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,54 @@
 #!/usr/bin/python3
-import re
+
 import sys
-from collections import defaultdict, Counter
-from signal import signal, SIGINT
 
-# Define the input format as a regular expression
-pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'
 
-# Initialize the variables to store tthe statistics
-total_size = 0
-status_codes = defaultdict(int)
+def print_statistics(status_code_counts, total_file_size):
+    """
+    Function to print statistics.
+    Args:
+        status_code_counts: Dictionary with counts
+        for each status code.
+    Returns:
+        None
+    """
 
-# Define a function to print the statistics
-def print_stats():
-    print("File size: {}".format(total_size))
-    for code, count in sorted(Counter(status_codes).items()):
-        print("{}: {}".format(code, count))
+    print("File size: {}".format(total_file_size))
+    for code, count in sorted(status_code_counts.items()):
+        if count != 0:
+            print("{}: {}".format(code, count))
 
-# Define a handler for the keyboard interruption
-def handler(signum, frame):
-    print_stats()
-    sys.exit(0)
 
-# Register the handler for the SIGINT signal
-signal(SIGINT, handler)
+total_file_size = 0
+counter = 0
+status_code_counts = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-# Read stdin line by line
-for line in sys.stdin:
-    # Match the line with the input format
-    match = re.search(pattern, line)
-    # If the line matches, extract the information
-    if match:
-        ip, date, status, size = match.groups()
-        # Update the statistics
-        total_size += int(size)
-        status_codes[status] += 1
-        # If 10 lines have been read, print the statistics and reset the counter
-        if line_count == 10:
-            print_stats()
-            line_count = 0
-    # If the line does not match, skip it
-    else:
-        continue
-    
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()[::-1]
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])
+                status_code = parsed_line[1]
+
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+
+            if counter == 10:
+                print_statistics(status_code_counts, total_file_size)
+                counter = 0
+
+finally:
+    print_statistics(status_code_counts, total_file_size)
