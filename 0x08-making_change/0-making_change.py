4a21c934a0e92dfcdb72b8f19c804b3d6546452a
Michaelkazembe <Michaelkazembe@gmail.com>
2024-02-02 00:40:01 +0200

    Efficient code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""
-Main file for testing
-"""
+"""Making Change Problem"""
 
 
 def makeChange(coins, total):
-    """ Main file for testing """
-    # if total is 0 or less, return 0
-    if total < 1:
+    """ Determines the fewest number of coins
+    needed to meet a given amount total"""
+    if total <= 0:
         return 0
 
-    # initialize an array to store the minimum number of coins
-    min_coins = [float('inf')] * (total + 1)
-
-    # the minimum number of coins to make 0 is 0
-    min_coins[0] = 0
-
-    # loop through the coins
+    currTotalCoins = 0
+    usedCoins = 0
+    coins = sorted(coins, reverse=True)
     for coin in coins:
-        # loop through the amounts from coin to total
-        for amount in range(coin, total + 1):
-            # update the minimum number of coins for the current amount
-            # by comparing the previous value and the value obtained by
-            # using the current coin
-            min_coins[amount] = min(min_coins[amount],
-                                    min_coins[amount - coin] + 1)
-
-    # return the minimum number of coins for the target amount
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+        r = (total - currTotalCoins) // coin
+        currTotalCoins += r * coin
+        usedCoins += r
+        if currTotalCoins == total:
+            return usedCoins
+    return -1
